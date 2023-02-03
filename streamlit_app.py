e2ae98d8be89129c237a4ea1b1df6136aeb49ef7
Akashhh1 <113428121+Akashhh1@users.noreply.github.com>
2023-02-03 13:12:32 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,6 @@ try:
 except URLError as e:
   streamlit.error()
     
-my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
