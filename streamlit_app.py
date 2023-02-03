0c7218e3509c2d5ceb5d2b2f26b75964b084606d
Akashhh1 <113428121+Akashhh1@users.noreply.github.com>
2023-02-03 12:55:40 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import pandas
 import requests
 from urllib.error import URLError
 
+streamlit.title('My Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
